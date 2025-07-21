dcd35f3eaee20b25b12a4a68e10c4d158e50ecf5
Marcos Vinícius <mvas.marcosvinicius@gmail.com>
2025-07-21 15:07:27 -0300

    Update filtragem_notas_emitidas.py

--- filtragem_notas_emitidas.py
+++ filtragem_notas_emitidas.py
@@ -5,7 +5,6 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import ElementNotInteractableException, TimeoutException, NoSuchElementException
-
 import pandas as pd
 import time
 
